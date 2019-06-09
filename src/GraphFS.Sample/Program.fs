46faacbf4a0a2178c923b499e8c11ed24ef204ca
Cameron Aavik <cameronaavik@gmail.com>
2019-06-09 19:01:18 +1000

    infinite grid example

--- src/GraphFS.Sample/Program.fs
+++ src/GraphFS.Sample/Program.fs
@@ -1,7 +1,8 @@
 ﻿open GraphFS.Graph
+open GraphFS.Core.VertexSet
+open GraphFS.Core.EdgeSet
 
-[<EntryPoint>]
-let main argv =
+let simpleExample =
     let g = 
         Graph.empty
         |> Graph.addEdge (0, 1)
@@ -10,5 +11,25 @@ let main argv =
 
     printfn "Nodes: %A" (Graph.verts g |> Seq.toList) // Nodes: [0; 1; 2; 3; 4]
     printfn "Neighbours of 2: %A" (Graph.neighbours 2 g |> Seq.toList) // Neighbours of 2: [3]
+    
 
+let infiniteGridExample =
+    let vertexSet = { new IVertexSet<int * int> with member __.HasVert _ = true}
+    let edgeSet = 
+        { new IEdgeSet<int * int> with
+            member __.HasEdge edge =
+                let (x1, y1), (x2, y2) = edge
+                let xDiff, yDiff = abs (x2 - x1), abs (y2 - y1)
+                (xDiff = 1 && yDiff = 0) || (xDiff = 0 && yDiff = 1)
+            member __.Neighbours p =
+                let x, y = p
+                upcast [| (x-1, y); (x+1, y); (x, y-1); (x, y+1) |] }
+    let gridGraph = new Graph<_, _, _>(vertexSet, edgeSet)
+    ()
+
+
+[<EntryPoint>]
+let main argv =
+    simpleExample |> ignore
+    infiniteGridExample |> ignore
     0
